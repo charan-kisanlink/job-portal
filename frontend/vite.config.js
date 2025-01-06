f1fd6c5d0cde28237f9c391c841ddf182a1c729c
Kisanlink <info@kisanlink.in>
2025-01-07 02:05:48 +0530

    fixes a comma typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     host: true,
     port: 443,
-  }
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

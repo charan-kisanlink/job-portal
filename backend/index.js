84d1f55f880264a2acd7ba02c8ea9b462caf650a
Kisanlink <info@kisanlink.in>
2025-01-13 00:02:24 +0530

    allows all cors

--- backend/index.js
+++ backend/index.js
@@ -28,12 +28,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:80",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
